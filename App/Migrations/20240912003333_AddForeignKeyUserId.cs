12d56f6d6f8aa5b6bc1c87431a213ec7de322a28
Fabricyo Barreto <bass.barreto@gmail.com>
2024-09-11 22:24:49 -0300

    feat: add migration AddColumnRoleToUsersTable

--- App/Migrations/20240912003333_AddForeignKeyUserId.cs
+++ App/Migrations/20240912003333_AddForeignKeyUserId.cs
@@ -20,7 +20,6 @@ namespace ApiTodo.App.Migrations
                 table: "Todos",
                 type: "uniqueidentifier",
                 nullable: false,
-                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
                 oldClrType: typeof(Guid),
                 oldType: "uniqueidentifier",
                 oldNullable: true);
---
+++ App/Migrations/20240912003630_AddColumnRoleToUsersTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTodo.App.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnRoleToUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "varchar(5)",
+                defaultValue: "User"
+            );
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Role_User_Admin",
+                table: "Users",
+                sql: "Role = 'User' or Role = 'Admin'"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "Role", table: "Users");
+            migrationBuilder.DropCheckConstraint(name: "CK_Role_User_Admin", table: "Users");
+        }
+    }
+}
