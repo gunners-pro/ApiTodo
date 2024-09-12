ec04dea5982ed0e3ec6095994d7b2cea073ee5cf
Fabricyo Barreto <bass.barreto@gmail.com>
2024-09-11 21:36:10 -0300

    feat: add migration AddForeignKeyUserId

---
+++ App/Migrations/20240912003333_AddForeignKeyUserId.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTodo.App.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_Users_UserId",
+                table: "Todos");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Todos",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_Users_UserId",
+                table: "Todos",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_Users_UserId",
+                table: "Todos");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Todos",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_Users_UserId",
+                table: "Todos",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
